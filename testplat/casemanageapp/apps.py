326eb12659fa0e79715a4ce86305871003bc6eb2
wuyajun <yajunwu@alog.com>
2019-09-25 10:47:10 +0800

    Initial commit

---
+++ testplat/casemanageapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CasemanageappConfig(AppConfig):
+    name = 'casemanageapp'
---
+++ testplat/datafactoryapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DatafactoryappConfig(AppConfig):
+    name = 'datafactoryapp'
